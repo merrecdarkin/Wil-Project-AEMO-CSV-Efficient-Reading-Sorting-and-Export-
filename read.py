51287f2285ae0ca6d51968e4a445748f18472f9b
Larry Huynh <doanhuynh12@protonmail.com>
2022-08-23 17:31:49 +0700

    Comments added

--- read.py
+++ read.py
@@ -17,18 +17,18 @@ from matplotlib.figure import Figure
 def loadCSV(nameOfCSV,DUIDsets,folPath):
     print(folPath)
     PathAndNameFile=folPath+"/"+nameOfCSV
-    df = pd.read_csv(PathAndNameFile, skiprows=1, skipfooter=1, on_bad_lines='skip')
+    df = pd.read_csv(PathAndNameFile, skiprows=1, skipfooter=1, on_bad_lines='skip')#This will fetch the BIDDAYOFFER_D
 
     for targetDUID in DUIDsets:
         print(df.loc[df['DUID'] == targetDUID])     
-    df2 = pd.read_csv(PathAndNameFile, skiprows=len(df. index)+2, skipfooter=1, on_bad_lines='skip')
+    df2 = pd.read_csv(PathAndNameFile, skiprows=len(df. index)+2, skipfooter=1, on_bad_lines='skip') #This will fetch the BIDPEROFFER_D 
     for targetDUID in DUIDsets:
         print(df2.loc[df2['DUID'] == targetDUID])     
 
 
 
 
-file_list_column = [
+file_list_column = [  #Most GUI layout will be done in this section
     [
         sg.Text("csv Folder"),
         sg.In(size=(25,1), enable_events=True, key="-FOLDER-"),
@@ -42,7 +42,7 @@ file_list_column = [
     ],
 ]
 
-input_column = [
+input_column = [ #These are input fiels and their corresponding fetch ID to be use later
     [sg.Text("User input pannel Placeholder")],
     [sg.Text(size=(40,1), key="-TOUT-")],
     [sg.Text("Target DUID (seperate via space)")],
@@ -65,13 +65,13 @@ layout = [
     ]
 ]
 
-window = sg.Window("File Comp",layout)
+window = sg.Window("File Comp",layout)  #This is GUI interaction function section
 
 while True:
     event, values = window.read()
     if event == "Exit" or event ==sg.WIN_CLOSED:
         break
-    if event== "-FOLDER-":
+    if event== "-FOLDER-": #This is GUI Folder button (currently perfect and need no change)
         folder= values["-FOLDER-"]
         try:
             file_list= os.listdir(folder)
@@ -93,9 +93,10 @@ while True:
         for f in file_list:
                 if os.path.isfile(os.path.join(folder, f))  and f.lower().endswith((".csv")) and ("PUBLIC_BIDMOVE_COMPLETE" in f):
                     print(f)
-                    if values["-INPUT DUID-"] != "":
-                        DUIDsets= values["-INPUT DUID-"].split()
+                    if values["-INPUT DUID-"] != "": 
+                        DUIDsets= values["-INPUT DUID-"].split() #This will fetch Value from inputs, require further expansion
                         loadCSV(f,DUIDsets,folder)
 
 
 window.close()
+
